0a664c74e6850b7989e2f2128cc81b65b3e208b6
28773 <2877369072@qq.com>
2020-12-17 18:52:48 +0800

    再次上传

--- app/src/main/java/com/example/day_03_class/MainActivity.java
+++ app/src/main/java/com/example/day_03_class/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.day_03_class;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.day_03_class.Personter.Personter;
+import com.example.day_03_class.adapter.InfoAdapter;
 import com.example.day_03_class.base.Item;
 import com.example.day_03_class.intergetr.IHome;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements IHome.View {
     private IHome.Personter personter;
+    private RecyclerView recy;
+    private List<Item.ResultsBean> list;
+    private InfoAdapter infoAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        personter=new Personter(this);
+        personter = new Personter(this);
+        initView();
         initDate();
     }
 
@@ -24,6 +35,17 @@ public class MainActivity extends AppCompatActivity implements IHome.View {
 
     @Override
     public void getItem(Item item) {
+        List<Item.ResultsBean> results = item.getResults();
+        list.addAll(results);
+        infoAdapter.notifyDataSetChanged();
 
+    }
+
+    private void initView() {
+        recy = (RecyclerView) findViewById(R.id.recy);
+        recy.setLayoutManager(new LinearLayoutManager(this));
+        list = new ArrayList<>();
+        infoAdapter = new InfoAdapter(list, this);
+        recy.setAdapter(infoAdapter);
     }
 }
--- app/src/main/java/com/example/day_03_class/adapter/InfoAdapter.java
+++ app/src/main/java/com/example/day_03_class/adapter/InfoAdapter.java
@@ -1,4 +1,29 @@
 package com.example.day_03_class.adapter;
 
-public class InfoAdapter  {
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.day_03_class.R;
+import com.example.day_03_class.base.Item;
+
+import java.util.List;
+
+public  class InfoAdapter extends BaseAdapter {
+    public InfoAdapter(List mDate, Context context) {
+        super(mDate, context);
+    }
+
+    @Override
+    protected int getLoght() {
+        return R.layout.layout;
+    }
+
+    @Override
+    protected void bind(Object date, VH vh) {
+        Item.ResultsBean resultsBean= (Item.ResultsBean) date;
+        ImageView iv_img = (ImageView) vh.getViewByid(R.id.iv_img);
+        Glide.with(vh.itemView).load(resultsBean.getUrl()).into(iv_img);
+    }
 }
