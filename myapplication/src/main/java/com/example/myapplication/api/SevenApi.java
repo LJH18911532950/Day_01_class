d02dcfe0253103c6f57637d951368a7a14ddad9d
28773 <2877369072@qq.com>
2020-12-17 19:12:04 +0800

    再次上传2

--- myapplication/src/main/java/com/example/myapplication/api/SevenApi.java
+++ myapplication/src/main/java/com/example/myapplication/api/SevenApi.java
@@ -1,5 +1,15 @@
 package com.example.myapplication.api;
 
+import android.content.ClipData;
+
+import com.example.myapplication.base.Result;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+
 public interface SevenApi {
+    String Url="https://gank.io/";
+    @GET("api/data/%E7%A6%8F%E5%88%A9/20/2")
+    Observable<Result>getResult();
 
 }
